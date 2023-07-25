71be9ae95874e24321c1528a3c089b214bdfce03
Alexey <riezievskii@mail.ru>
2023-07-25 14:46:16 +0300

    cart...

--- shop/shop/settings.py
+++ shop/shop/settings.py
@@ -36,14 +36,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'myshop.apps.MyshopConfig',
+    'cart.apps.CartConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'myshop',
-    'cart',
 ]
 
 MIDDLEWARE = [
